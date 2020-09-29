b7cbaa6638d064792e7ce8207bcaf3ac3ce129f5
Matthew Dahl <64293526+MatthewDahl@users.noreply.github.com>
2020-09-29 08:30:01 -0700

    add button navigation

--- src/app/dashboard/beneficiaries/add/add.component.ts
+++ src/app/dashboard/beneficiaries/add/add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-add',
@@ -6,7 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add.component.css'],
 })
 export class AddComponent implements OnInit {
-  constructor() {}
+  lastStep = false;
+  currentStep: string;
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {}
+
+  handleAction(action: 'next' | 'prev'): void {
+    if (this.lastStep) {
+      // Handle submit data
+      return;
+    }
+
+    const nextStep = {
+      '/dashboard/beneficiaries/add/select': {
+        next: 'details',
+        prev: '/dashboard/beneficiaries',
+      },
+      '/dashboard/beneficiaries/add/details': {
+        next: 'allocations',
+        prev: 'select',
+      },
+      '/dashboard/beneficiaries/add/allocations': {
+        next: null,
+        prev: 'details',
+      },
+    };
+    this.router.navigate([nextStep[this.router.url][action]], { relativeTo: this.route });
+  }
 }
