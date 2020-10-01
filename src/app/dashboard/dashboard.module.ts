836d033a99490fea1564d1e6fcdd269b46bba7ed
Matthew Dahl <64293526+MatthewDahl@users.noreply.github.com>
2020-10-01 14:00:25 -0700

    import OverViewModule in DashboardModule

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardNavModule } from '../shared/dashboard-nav/dashboard-nav.module';
+import { OverviewModule } from './overview/overview.module';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, DashboardRoutingModule, DashboardNavModule],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    DashboardNavModule,
+    OverviewModule,
+  ],
 })
 export class DashboardModule {}
