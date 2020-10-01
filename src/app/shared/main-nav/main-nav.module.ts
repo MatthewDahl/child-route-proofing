1b927c3cbbae1b4633d857c8442cb449b163c9c7
Matthew Dahl <64293526+MatthewDahl@users.noreply.github.com>
2020-10-01 11:32:46 -0700

    refactor main-nav to use fab-mini-button

--- src/app/shared/main-nav/main-nav.module.ts
+++ src/app/shared/main-nav/main-nav.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MainNavComponent } from './main-nav.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [MainNavComponent],
-  imports: [CommonModule, MatToolbarModule, MatButtonModule, RouterModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule,
+  ],
   exports: [MainNavComponent],
 })
 export class MainNavModule {}
