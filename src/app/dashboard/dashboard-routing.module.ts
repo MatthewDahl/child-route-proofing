c3bf3f0e86759ec37bb43f191f62f77adafd401e
Matthew Dahl <64293526+MatthewDahl@users.noreply.github.com>
2020-10-01 13:56:49 -0700

    add root dashboard child component

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard.component';
+import { OverviewComponent } from './overview/overview.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
     children: [
+      {
+        path: '',
+        component: OverviewComponent,
+      },
       {
         path: 'payments',
         loadChildren: () =>
